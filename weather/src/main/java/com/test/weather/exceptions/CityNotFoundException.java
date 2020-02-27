5328c911143c99cfda200597194d9cf229a80e38
Nemocoder <denisidorenko@gmail.com>
2020-02-27 20:51:19 +0500

    Remove

--- weather/src/main/java/com/test/weather/exceptions/CityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.test.weather.exceptions;
-
-public class CityNotFoundException extends RuntimeException {
-    public CityNotFoundException(String s){
-        System.out.println(s);
-    }
-}
