d5e15969da4e13ea5f83c3d2663a7d05b767143f
Nemocoder <denisidorenko@gmail.com>
2020-02-28 12:14:13 +0500

    remove old

--- weather/src/main/java/com/test/weather/repository/WeatherRepository.java
+++
@@ -1,8 +0,0 @@
-package com.test.weather.repository;
-
-import com.test.weather.entity.Weather;
-import org.springframework.data.repository.CrudRepository;
-
-public interface WeatherRepository extends CrudRepository<Weather, String> {
-    Weather findByCityAndService (String city, String service);
-}
